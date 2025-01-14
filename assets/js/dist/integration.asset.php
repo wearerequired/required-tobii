b24fdfaaf74c2a61c60da82c4f4cc289641d780f
krokodok <jeff@required.ch>
2025-01-14 10:13:29 +0100

    Transfer HTML formatting to lightbox caption

--- assets/js/dist/integration.asset.php
+++ assets/js/dist/integration.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '2a2f45f5f08cdf967e5a');
+<?php return array('dependencies' => array(), 'version' => 'c4f5245a3cbc8f1d809f');
--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -4,16 +4,17 @@ import './style.scss';
 
 new Tobii( {
 	selector: '[data-lightbox]',
+	captionHTML: true,
 	captionText: ( element ) => {
 		let sibling = element.nextElementSibling;
 		if ( sibling && 'FIGCAPTION' === sibling.tagName ) {
-			return sibling.textContent.trim();
+			return sibling.innerHTML.trim();
 		}
 
 		// In classic editor, a gallery image is wrapped in an additional div container.
 		sibling = element.parentNode.nextElementSibling;
 		if ( sibling && 'FIGCAPTION' === sibling.tagName ) {
-			return sibling.textContent.trim();
+			return sibling.innerHTML.trim();
 		}
 
 		return null;
