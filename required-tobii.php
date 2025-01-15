bc764d3179b252c4cd52ec0ceebf446b7beaab09
Dominik Schilling <dominikschilling+git@gmail.com>
2025-01-15 10:50:11 +0100

    Bump version

--- required-tobii.php
+++ required-tobii.php
@@ -3,13 +3,13 @@
  * Plugin Name: required Tobii
  * Plugin URI:  https://github.com/wearerequired/required-tobii
  * Description: An accessible, open-source lightbox for WordPress.
- * Version:     2.0.0-rc.1
+ * Version:     2.0.0-rc.2
  * Author:      required
  * Author URI:  https://required.com
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
- * Copyright (c) 2020-2024 required (email: info@required.com)
+ * Copyright (c) 2020-2025 required (email: info@required.com)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2 or, at
